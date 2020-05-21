a67139263c36dddfc98a1db22d802b919171e288
liuzhiqiang <1964252235@qq.com>
2020-05-21 11:37:09 +0800

    main

--- main.go
+++ main.go
@@ -1,44 +1,49 @@
 package main
 
 import (
+	"email/routers"
+	config "email/utils/conf"
+	"email/utils/dbutil"
+	etcdIni "email/utils/etcd"
+	"email/utils/redisUtil"
 	"fmt"
-	"net"
+	"runtime"
 )
 
 func main() {
-	//runtime.GOMAXPROCS(runtime.NumCPU())
-	//etcdIni.InitEtcd()
-	//err := redisUtil.InitRedis(config.Conf)
-	//if err != nil {
-	//	fmt.Println(err)
-	//	return
-	//}
-	//err = dbutil.InitDb(config.Conf)
-	//if err != nil {
-	//	fmt.Println(err)
-	//	return
-	//}
-	//routers.StartHttpServer(18080)
-
-	fmt.Println("running,not deadlock")
-	waitQueue := make(chan int)
-	waitQueue <- 1
-	go func() {
-		<- waitQueue
-	}()
-	close(waitQueue)
-	server,err := net.Listen("tcp","localhost:8080")
-	if err != nil{
-		panic(err)
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	etcdIni.InitEtcd()
+	err := redisUtil.InitRedis(config.Conf)
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
-	for {
-		conn,err := server.Accept()
-		if err!=nil{
-			panic("server")
-		}
-		fmt.Printf("rec %s",conn.RemoteAddr())
-
+	err = dbutil.InitDb(config.Conf)
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
+	routers.StartHttpServer(18080)
+
+	//fmt.Println("running,not deadlock")
+	//waitQueue := make(chan int)
+	//waitQueue <- 1
+	//go func() {
+	//	<- waitQueue
+	//}()
+	//close(waitQueue)
+	//server,err := net.Listen("tcp","localhost:8080")
+	//if err != nil{
+	//	panic(err)
+	//}
+	//for {
+	//	conn,err := server.Accept()
+	//	if err!=nil{
+	//		panic("server")
+	//	}
+	//	fmt.Printf("rec %s",conn.RemoteAddr())
+	//
+	//}
 
 
 }
